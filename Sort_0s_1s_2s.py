4bf733b4d7976213e800c27138765cb9d58a45af
NecronTrooper <akezhan.kumarov@gmail.com>
2025-04-24 16:50:15 +0500

    truing my best

--- Sort_0s_1s_2s.py
+++ Sort_0s_1s_2s.py
@@ -1,6 +1,6 @@
 #Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
 #You need to solve this problem without utilizing the built-in sort function.
-def bubble_sort(arr):
+def bubble_sort(arr):     # Doesn't fit by requirement, need optimization
     n = len(arr)
     for i in range(n):
         for j in range(n-1-i):
@@ -11,8 +11,45 @@ def bubble_sort(arr):
 
     return arr
 
+def Dutch_national_flag_algo(arr):  # The Dutch National Flag Algorithm is a sorting algorithm that divides an array into three segments 
+                                    # based on three distinct values, often referred to as low, mid, and high. 
+                                    # The algorithm efficiently rearranges elements so that all low elements come first, 
+                                    # followed by mid elements, and finally, all high elements.
+    
+    low = 0
+    mid = 0
+    high = len(arr) - 1 
+    
+    temp = 0
+
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            temp = arr[low]
+            arr[low] = arr[i]
+            arr[i] = temp
+            
+            low = low + 1
+        
+        if arr[i] == 2:
+            if arr[high] == 2:
+                high = high - 1
+            temp = arr[high]
+            arr[high] = arr[i]
+            arr[i] = temp
+
+            high = high - 1
+        print(arr)    
+
+
+
+    return arr
+
+
 arr1 = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
 arr2 = [0, 1, 2, 0, 1, 2]
 
-print(bubble_sort(arr1))
-print(bubble_sort(arr2))
+print(Dutch_national_flag_algo(arr1))
+print(Dutch_national_flag_algo(arr2))
+
+
+
